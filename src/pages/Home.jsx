64a2f9cc22fcfb0913cd4f9585c90d48581afea8
Akhil santhosh <akhilsanthosh043@gmail.com>
2025-11-17 23:52:25 +0530

    home ui changed and centered jobcard text in job page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,24 +18,27 @@ function Home() {
   const latestjobs = jobs.slice(0, 4);
   return (
     <div className=" text-center m-5">
-      <p className="font-bold text-3xl mt-20 ">Find Your Dream IT Jobs</p>
-      <div className="p-2 text-xl font-semibold mt-5">
+      <p className="font-extrabold text-gray-800 text-4xl mt-20 ">Find Your Dream IT Jobs</p>
+      <div className="p-2 text-xl font-semibold mt-5 text-gray-600">
         Stay updated with the newest openings from Kerala’s top IT hubs —
         Infopark and Technopark.
       </div>
-      <div className="p-2 font-semibold text-lg">
+      <div className="p-2 text-gray-500">
         Your one-stop portal for IT jobs in Kochi, Trivandrum, and beyond.
       </div>
-      <div className="grid grid-cols-4 gap-4 pt-5">
+      
+      <div className="grid grid-cols-4 gap-4  max-w-6xl mx-auto  mt-10">
         {latestjobs.map((job) => (
           <JobCard key={job.id} job={job} />
         ))}
       </div>
-      <Link to={"/Jobs"}>
-        <button className="p-4 bg-purple-400 rounded-xl mt-4 mb-4 hover:bg-indigo-400">
-          Explore Jobs
-        </button>
-      </Link>
+      <div className="mt-10">
+        <Link to={"/Jobs"}>
+          <button className="py-3 px-6 bg-indigo-500 text-white font-semibold shadow-md text-lg rounded-xl hover:text-indigo-100 transition ">
+            Explore Jobs
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
+
 import JobList from '../components/JobList'
-import { getAllJobs,getByTitle } from "../services/JobServices";
-import SearchBar from '../components/SearchBar';
+
 function Job() {
    
   return (
-    <div className='p-10'>
+    <div className='p-10 text-center'>
     
       <JobList />
     </div>
